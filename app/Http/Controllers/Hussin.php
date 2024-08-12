6ff987defbcbaed3c24f0344e11141666f3f0582
Hussein Yasser <119596187+Hussein2077@users.noreply.github.com>
2024-08-13 00:43:27 +0300

    posts

--- app/Http/Controllers/Hussin.php
+++ app/Http/Controllers/Hussin.php
@@ -6,6 +6,15 @@ use Illuminate\Http\Request;
 
 class Hussin extends Controller
 {
-
-    //
+    public function index()
+    {
+        $allPosts = [
+            ['id' => 1 , 'title' => 'PHP', 'posted_by' => 'Ahmed', 'created_at' => '2022-10-10 09:00:00'],
+            ['id' => 2 , 'title' => 'Javascript', 'posted_by' => 'Mohamed', 'created_at' => '2023-08-20 07:00:00'],
+            ['id' => 3 , 'title' => 'HTML', 'posted_by' => 'Mahmoud', 'created_at' => '2023-10-06 06:00:00'],
+            ['id' => 4 , 'title' => 'CSS', 'posted_by' => 'Ali', 'created_at' => '2023-08-07 05:00:00'],
+        ];
+        return view('test', ['posts' => $allPosts]);
+    }
 }
+ 
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
+use App\Http\Controllers\Hussin;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/test', function () {
-    return view('test', [
-        'foo' => 'sss',
-        'bar' => 'ssssssss',
-        'baz' => 'vvvv',
-        'qux' => 'quux',
-    ]);
-});
+ 
+
+Route::get('/posts',[Hussin::class, 'testAction']);
+//1- define a new route so the user can access it through browser
+//2- define controller that renders a view
+//3- define view that contains list of posts
+//4- remove any static html data from the view
